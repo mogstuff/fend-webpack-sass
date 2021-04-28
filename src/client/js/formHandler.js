f2b8ee43c2fbf2f78ce3976efdadc57a43687c55
mogstuff <markmorgan.mcp@gmail.com>
2021-04-28 11:16:09 +0100

    feat: api call challenge

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,19 +1,41 @@
 function handleSubmit(event) {
     event.preventDefault()
 
-    // check what text was put into the form field
     let formText = document.getElementById('name').value
 
-    Client.checkForName(formText)
+    const apiKey = '4fc7c2f853c096220e99e961df027e50';
+
+    let url =  'https://api.openweathermap.org/data/2.5/weather?' + 'q=' + formText + ',uk' + '&units=metric' +  '&appid=' + apiKey;
+
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    fetch(url)
     .then(res => {
         return res.json()
     })
     .then(function(data) {
-        document.getElementById('results').innerHTML = data.message
+
+        console.log('data here:');
+        console.log(data);
+
+        let results = document.getElementById('results');
+
+        let h2 = document.createElement('h2');
+        h2.innerText = 'Weather Today';
+
+        let h3 = document.createElement('h3');
+        h3.innerText = 'Location: ' + data.name;
+
+        let temp = document.createElement('div');
+        temp.innerText = 'Temperature (C) : ' + data.main.temp;
+
+        results.appendChild(h2);
+        results.appendChild(h3);
+        results.appendChild(temp);
+
     })
 }
 
 export { handleSubmit }
+
+
